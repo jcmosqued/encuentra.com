12f06f95a0a4d43ffd915e15346ad60f93ca443e
Berenice Alonso <bere_alonsoc@hotmail.com>
2018-03-13 21:54:50 -0600

    Página de anuncio - version 1

---
+++ js/mapaMiniatura.js
@@ -0,0 +1,7 @@
+function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('mapaMiniatura'), {
+          center: {lat: 21.12048674253761, lng: -101.67953196892546},
+          zoom: 15
+        });
+}
